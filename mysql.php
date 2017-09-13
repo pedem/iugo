22304a13ffebe18255042bc3cf44fa5a114b99b7
Michael Pede <Michael Pede@applications.localhost.brightmls.com>
2017-09-13 13:53:33 -0700

    PDO DB - test server setup

--- mysql.php
+++ mysql.php
@@ -3,12 +3,34 @@ $servername = "localhost";
 $username = "mpede_iugo";
 $password = "notsecret";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+try {
+    $db = new PDO("mysql:host=$servername;dbname=iugo", $username, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+
+try {
+    // First of all, let's begin a transaction
+    $db->beginTransaction();
+
+    // A set of queries; if one fails, an exception should be thrown
+    $db->query('first query');
+    $db->query('second query');
+    $db->query('third query');
+
+    // If we arrive here, it means that no exception was thrown
+    // i.e. no query has failed, and we can commit the transaction
+    $db->commit();
+} catch (Exception $e) {
+	echo "Query failed: " . $e->getMessage();
+    // An exception has been thrown
+    // We must rollback the transaction
+    $db->rollback();
+}
 ?>
