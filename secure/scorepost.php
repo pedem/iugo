d8799b3054d2866415cfda89621c549c6b317c7c
Michael Pede <Michael Pede@applications.localhost.brightmls.com>
2017-09-14 11:28:25 -0700

    Postdata needed

--- secure/scorepost.php
+++ secure/scorepost.php
@@ -1,6 +1,8 @@
 <?php
 require("leaderboardMgr.php");
 
+$postData = json_decode(file_get_contents('php://input'), true);
+
 $ldr = new LeaderBoardManager;
 $ldr->insertLeaderEntryFromPost($postData);
 
