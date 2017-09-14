fd201f258ab031010e65b5b29760daa5ea931e85
Michael Pede <Michael Pede@applications.localhost.brightmls.com>
2017-09-13 19:02:21 -0700

    Made into an Object

--- controller.php
+++ controller.php
@@ -2,18 +2,47 @@
 // Everything we're doing is JSON
 header('Content-type: text/javascript');
 
+// The allowed paths to modules in the secure folder
 $allowed = array(
 	"transactionstats" => true,
 	"transaction" => true,
 	"timestamp" => true
 );
+
+class Controller
+{
+	// Privvate Constants
+	private const SECURE_PATH = "secure";
+	private const INVALID_PATH = "Invalid URL, Please consult Server Specification Ducument.";
+
+	private $allowed;
+	private $path
+
+	public function __construct($path)
+	{
+		$this->path = $path;
+	}
+
+	public function isAllowed()
+	{
+		return $allowed[$this->path];
+	}
+
+	public function run()
+	{
+		require(self::SECURE_PATH . "/$path.php");
+	}
+}
+
 $path = strtolower( stripslashes( trim( $_GET['path'] ) ) );
+
 try
 {
-	if ($allowed[$path])
-		require("secure/$path.php");
+	$ctrl = new Controller($path);
+	if ($ctrl->isAllowed())
+		$ctrl->run();
 	else
-		throw new Exception("Invalid URL, Please consult Server Specification Ducument.");
+		throw new Exception(Controller::INVALID_PATH);
 }
 catch (Exception $e)
 {
